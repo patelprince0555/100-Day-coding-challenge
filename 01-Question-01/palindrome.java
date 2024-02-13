0db896ce6db3607c97a82792a9d32791ee9307dc
patelprince0555 <patelprince0555@gmail.com>
2024-02-13 13:57:51 +0530

    13-02-2024

---
+++ 01-Question-01/palindrome.java
@@ -0,0 +1,26 @@
+// Write a Java program to check if a number is a palindrome in Java
+
+import java.util.Scanner;
+
+public class palindrome{
+    public static int ispalindrome(int n){
+        int reverse=0;
+       while(n!=0){
+        int lastdigit = n%10;
+        reverse=reverse*10+lastdigit;
+        n=n/10;
+       }
+       return reverse;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int number1 = sc.nextInt();
+        if(ispalindrome(number1)==number1){
+            System.out.println("number:"+number1+"is a palindrome");
+        }else{
+            System.out.println("number:"+number1+"is not a palindrome");
+        }
+        
+    }
+}
