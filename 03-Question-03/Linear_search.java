f13e4145d27eac9e2c3f555e65afaf4ac2c0d0ab
patelprince0555 <patelprince0555@gmail.com>
2024-06-02 20:53:32 +0530

    02-06-2024

--- 03-Question-03/Linear_search.java
+++ 03-Question-03/Linear_search.java
@@ -1,6 +1,15 @@
 // linear serch
 public class Linear_search {
+    public static int linearSearch(int n[],int key){
+        for(int i =0;i<n.length;i++){
+            if(n[i]==key){
+                return i;
+            }
+        }
+        return-1;
+    }
     public static void main(String[] args) {
-        
+       int numbers[]={2,4,6,8,10,12,14,16}; 
+       System.out.println(linearSearch(numbers, 10));              
     }
 }
