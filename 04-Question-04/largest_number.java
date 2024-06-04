28be8cf7527218fc187a8de8fefb12d8f111ea13
patelprince0555 <patelprince0555@gmail.com>
2024-06-04 23:16:59 +0530

    04-06-2024

---
+++ 04-Question-04/largest_number.java
@@ -0,0 +1,16 @@
+
+public class largest_number {
+    public static int largestnumber(int numbers[]){
+        int largest=Integer.MIN_VALUE;
+        for(int i =0;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest=numbers[i];
+            }
+        }
+        return largest;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,2,3,10,5,6};
+        System.out.println(largestnumber(numbers));
+    }
+}
