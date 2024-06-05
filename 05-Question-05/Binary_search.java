bd04b89bd8dce42c0dc3e380ee1cf362c2c57a6e
patelprince0555 <patelprince0555@gmail.com>
2024-06-05 12:10:25 +0530

    05-06-2024

---
+++ 05-Question-05/Binary_search.java
@@ -0,0 +1,24 @@
+// Binary search
+public class Binary_search {
+    public static int Binarysearch(int numbers[],int key){
+        int start=0;
+        int end = numbers.length-1;
+        while (start<=end) {
+            int mid=(start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            }
+            if(numbers[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return-1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,14,16};
+        int key=10;
+        System.out.println(Binarysearch(numbers,key));
+    }
+}
