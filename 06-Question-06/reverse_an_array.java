84ff752729ba1f7cc9894386318527702be5ac65
patelprince0555 <patelprince0555@gmail.com>
2024-06-07 00:08:41 +0530

    07-06-2024

--- 06-Question-06/reverse_an_array.java
+++ 06-Question-06/reverse_an_array.java
@@ -1,5 +1,21 @@
+// Reverse an array
 public class reverse_an_array {
+    public static void reversearray(int numbers[]){
+        int start =0;
+        int end = numbers.length-1;
+         while (start<=end) {
+            int temp=numbers[end];
+            numbers[end]=numbers[start];
+            numbers[start]=temp;
+            start++;
+            end--;
+         }
+    }
     public static void main(String[] args) {
-        
+        int numbers[]={2,3,4,3,8,4};
+         reversearray(numbers);
+         for(int i =0;i<numbers.length;i++){
+            System.out.print(numbers[i]+" ");
+         }
     }
 }
