5c91cc2c184a5a4e2c88e603ac278f1b5d2d3d11
patelprince0555 <patelprince0555@gmail.com>
2024-06-29 18:07:03 +0530

    29-06-2024

---
+++ 09-Question-09/Max_SubArray_sum.java
@@ -0,0 +1,27 @@
+// max subarray sum
+public class Max_SubArray_sum {
+    public static void maxSubarraysum(int numbers[]){
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            int start=i;
+            for(int j= i;j<numbers.length;j++){
+                int end=j;
+                currsum=0;
+                for(int k= start;k<=end;k++){
+                    currsum+=numbers[k];
+                }
+                System.out.print(currsum+" ");
+                if(maxsum<currsum){
+                    maxsum=currsum;
+                }
+            }
+            System.out.println();
+        }
+        System.out.print(maxsum);
+    }
+    public static void main(String[] args) {
+    int num[]={1,-2,6,-1,3};
+    maxSubarraysum(num);
+    }
+}
