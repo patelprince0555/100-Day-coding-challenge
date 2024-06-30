e4cd731fe18a5d72bf5c87e22f534b2329b72b8b
patelprince0555 <patelprince0555@gmail.com>
2024-06-30 12:38:22 +0530

    30-06-2024

--- 10-Question-10/max_subarray_sum.java
+++ 10-Question-10/max_subarray_sum.java
@@ -1,7 +1,28 @@
 // max subarray sum (prefix array)
 
 public class max_subarray_sum {
+    public static void MaxSubarraySum(int numbers[]){
+        int currsum=0;
+        int maxsum=Integer.MIN_VALUE;
+        int prefix[]= new int[numbers.length];
+        prefix[0]=numbers[0];
+        for(int i=1;i<prefix.length;i++){
+            prefix[i]=prefix[i-1]+numbers[i];
+        }
+        for(int i =0;i<numbers.length;i++){
+            int start=i;
+            for(int j =i;j<numbers.length;j++){
+                int end=j;
+                currsum=start==0?prefix[end]:prefix[end]-prefix[start-1];
+                if(maxsum<currsum){
+                    maxsum=currsum;
+                }
+            }
+        }
+        System.out.println(maxsum);
+    }
     public static void main(String[] args) {
-        int num1[]={1,-2,6,-1};
+        int num1[]={1,-2,6,-1,3};
+        MaxSubarraySum(num1);
     }
 }
