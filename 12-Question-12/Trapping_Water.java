a1174e93a9a668ce2b65bf8a24603ad7378469c7
patelprince0555 <patelprince0555@gmail.com>
2024-07-24 20:30:43 +0530

    24-07-2024

--- 12-Question-12/Trapping_Water.java
+++ 12-Question-12/Trapping_Water.java
@@ -1,7 +1,34 @@
 // Trapping water
 
 public class Trapping_Water {
+  public static int TrappingWater(int height[]){
+     int n =height.length;
+
+     // leftmax boundary
+     int leftMax[]=new int[n];
+     leftMax[0]=height[0];
+     for(int i=1;i<n;i++){
+        leftMax[i]=Math.max(height[i], leftMax[i-1]);
+     }
+
+     // rightMax Boundary
+     int rightmax[]=new int[n];
+     rightmax[n-1]=height[n-1];
+     for(int i=n-2;i>=0;i--){
+        rightmax[i]=Math.max(height[i], rightmax[i+1]);
+     }
+
+     int trappedWater=0;
+     for(int i=0;i<n;i++){
+        int waterLevel=Math.min(leftMax[i],rightmax[i]);
+        trappedWater+=waterLevel-height[i];
+     }
+     return trappedWater;
+  }
+       
     public static void main(String[] args) {
+        int height[]={4,2,0,6,3,2,5};
+        System.out.println(TrappingWater(height));
         
     }
 }
