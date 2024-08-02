6e7f90c3bcd66b8bd089ba5af4b465136d904135
patelprince0555 <patelprince0555@gmail.com>
2024-08-02 17:40:50 +0530

    02-08-2024

--- 14-Question-14/Remove_Element.java
+++ 14-Question-14/Remove_Element.java
@@ -1,3 +1,4 @@
+// Remove Element
 // Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
 
 // Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
@@ -46,7 +47,18 @@
 // 0 <= val <= 100
 
 public class Remove_Element {
+    public static int removeElement(int nums[],int val){
+    int count=0;
+    for(int i=0;i<nums.length;i++){
+        if(nums[i]!=val){
+            nums[count]=nums[i];
+            count++;
+        }
+    }
+    return count;
+}
     public static void main(String[] args) {
-        
+        int nums[]={3,2,2,3};
+        System.out.println(removeElement(nums, 3));
     }
 }
