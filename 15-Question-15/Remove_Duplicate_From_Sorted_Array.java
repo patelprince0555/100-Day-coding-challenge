1f0e2669632565a15bab74660e231259712a7a2b
patelprince0555 <patelprince0555@gmail.com>
2024-08-02 17:55:09 +0530

    02-08-2024

--- 15-Question-15/Remove_Duplicate_From_Sorted_Array.java
+++ 15-Question-15/Remove_Duplicate_From_Sorted_Array.java
@@ -46,7 +46,21 @@
 
 
 public class Remove_Duplicate_From_Sorted_Array {
+    public static  int RemoveDuplicates(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(i<nums.length-1 && nums[i]==nums[i+1]){
+                continue;
+            }else{
+                nums[count]=nums[i];
+                count++;         
+            }
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
-        
+        int nums[]={0,0,1,1,1,2,2,3,3,4};
+        System.out.println(RemoveDuplicates(nums));
     }
 }
