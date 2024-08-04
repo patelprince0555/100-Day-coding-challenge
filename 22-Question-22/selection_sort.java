382b8e748221fb7fe51f78048f533c6c0c0650fe
patelprince0555 <patelprince0555@gmail.com>
2024-08-05 00:18:52 +0530

    05-08-2024

---
+++ 22-Question-22/selection_sort.java
@@ -0,0 +1,10 @@
+// selection-sort
+
+public class selection_sort {
+    public static void selectionSort(int nums[]){
+        
+    }
+    public static void main(String[] args) {
+        int nums[]={5,4,3,1,2};
+    }
+}
