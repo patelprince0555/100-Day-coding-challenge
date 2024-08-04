7002ba8f9863f85b903175c5e7e42bfbe2cf75c0
patelprince0555 <patelprince0555@gmail.com>
2024-08-05 00:15:56 +0530

    05-08-2024

--- 21-Question-21/Bubble_sort.java
+++ 21-Question-21/Bubble_sort.java
@@ -1,7 +1,29 @@
 // bubble sort
 
 public class Bubble_sort {
+    public static void bubbleSort(int nums[]){
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=0;j<nums.length-1-i;j++){
+                if(nums[j]<nums[j+1]){
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+
+                }
+            }
+        }
+    }
+
+    public static  void printArray(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
+        int nums[]={5,4,1,3,2};
+        bubbleSort(nums);
+        printArray(nums);
         
     }
 }
