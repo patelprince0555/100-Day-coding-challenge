9bb6abef73a47b269c2e4b79bc973f25051bbd81
patelprince0555 <patelprince0555@gmail.com>
2024-08-05 00:40:53 +0530

    05-08-2024

--- 22-Question-22/selection_sort.java
+++ 22-Question-22/selection_sort.java
@@ -2,9 +2,25 @@
 
 public class selection_sort {
     public static void selectionSort(int nums[]){
-        
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]>nums[j]){
+                    int temp=nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                }
+            }
+        }
+    }
+    public static  void printArray(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+        System.out.println();
     }
     public static void main(String[] args) {
         int nums[]={5,4,3,1,2};
+        selectionSort(nums);
+        printArray(nums);
     }
 }
