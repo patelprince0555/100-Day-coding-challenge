5020a21b162590550716dee668c8454d20b8ec22
patelprince0555 <patelprince0555@gmail.com>
2024-08-05 19:54:34 +0530

    05-08-2024

---
+++ 24-Question-24/Second_Smallest_number.java
@@ -0,0 +1,29 @@
+// find second smallest element in an array
+// Problem Statement: Given an array, find the second smallest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+// Example 1:
+// Input:
+//  [1,2,4,7,7,5]
+// Output:
+//  Second Smallest : 2
+
+public class Second_Smallest_number {
+    public static void SecondSmallest(int nums[]){
+        int min=Integer.MAX_VALUE;
+        int second_min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                second_min=min;
+                min=nums[i];
+            }else if (nums[i]<second_min) {
+                second_min=nums[i];
+            } else if(second_min==min){
+                second_min=-1;
+            }
+        }
+        System.out.println("second smallest number is: "+second_min);
+    }
+    public static void main(String[] args) {
+        int nums[]={2,10,8,1,6,9};
+        SecondSmallest(nums);
+    }
+}
