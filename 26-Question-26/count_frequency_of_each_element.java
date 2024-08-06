21c5ebdcde079597d7b76e3dbbc1a4f584852a86
patelprince0555 <patelprince0555@gmail.com>
2024-08-06 17:19:17 +0530

    06-08-2024

---
+++ 26-Question-26/count_frequency_of_each_element.java
@@ -0,0 +1,7 @@
+// count frequency of each element in an array
+
+public class count_frequency_of_each_element {
+    public static void main(String[] args) {
+        int nums[]={10,5,10,15,10,5};
+    }
+}
