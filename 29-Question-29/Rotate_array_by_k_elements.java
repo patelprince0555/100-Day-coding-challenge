bf3fcf35525352bf68f450ed1590f88e3de7a59e
patelprince0555 <patelprince0555@gmail.com>
2024-08-08 14:01:47 +0530

    08-08-2024

--- 29-Question-29/Rotate_array_by_k_elements.java
+++ 29-Question-29/Rotate_array_by_k_elements.java
@@ -14,7 +14,31 @@
 // Explanation: Rotate the array to right by 3 elements.
 
 public class Rotate_array_by_k_elements {
+    public  static void reverse(int nums[],int start,int end){
+        while(start<=end){
+            int temp=nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public  static void rotate(int[] nums, int k) {
+        int n =nums.length;
+        k%=n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k);
+        reverse(nums,k+1,n-1);
+    }
+
+    public static void printArray(int nums[]){
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
     public static void main(String[] args) {
         int nums[]={1,2,3,4,5,6,7};
+        rotate(nums, 3);
+        printArray(nums);
     }
 }
