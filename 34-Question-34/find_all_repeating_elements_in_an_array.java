32200133b8595a1af5c0f4840d714b5cf72600ff
patelprince0555 <patelprince0555@gmail.com>
2024-08-11 01:52:32 +0530

    11-08-2024

---
+++ 34-Question-34/find_all_repeating_elements_in_an_array.java
@@ -0,0 +1,26 @@
+//Find all repeating elements in an array
+// Problem Statement: Find all the repeating elements present in an array.
+
+// Examples:
+
+// Example 1:
+// Input: 
+// Arr[] = [1,1,2,3,4,4,5,2]
+// Output:
+//  1,2,4
+// Explanation:
+//  1,2 and 4 are the elements which are occurring more than once.
+
+// Example 2:
+// Input:
+//  Arr[] = [1,1,0]
+// Output:
+//  1
+// Explanation:
+//  Only 1 is occurring more than once in the given array.
+
+public class find_all_repeating_elements_in_an_array {
+    public static void main(String[] args) {
+        
+    }
+}
