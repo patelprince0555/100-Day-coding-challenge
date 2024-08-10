8ed43604b044843f6a02c343a64212615bea4f47
patelprince0555 <patelprince0555@gmail.com>
2024-08-11 02:04:19 +0530

    11-08-2024

--- 34-Question-34/find_all_repeating_elements_in_an_array.java
+++ 34-Question-34/find_all_repeating_elements_in_an_array.java
@@ -20,7 +20,36 @@
 //  Only 1 is occurring more than once in the given array.
 
 public class find_all_repeating_elements_in_an_array {
+    public static int[] allRepeating(int arr[]){
+        int n= arr.length;
+        int count=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[j]==arr[i]){
+                    arr[count]=arr[i];
+                    count++;
+                }
+            }
+        }
+         // Create a new array with the correct size
+         int[] result = new int[count];
+         for (int i = 0; i < count; i++) {
+             result[i] = arr[i];
+         }
+ 
+         return result;
+    }
+
+    
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int nums[]={1,1,2,3,4,4,5,2};
+       int nums1[] =allRepeating(nums);
+        printArray(nums1);
     }
 }
