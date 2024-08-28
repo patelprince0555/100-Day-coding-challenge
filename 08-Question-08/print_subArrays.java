f12f087e347f6aa3761c887c3f0351789ab0971f
patelprince0555 <patelprince0555@gmail.com>
2024-08-28 13:23:20 +0530

    28-08-2024

--- 08-Question-08/print_subArrays.java
+++ 08-Question-08/print_subArrays.java
@@ -2,6 +2,7 @@
 
 public class print_subArrays {
     public static void printSubarray(int numbers[]){
+        int tr=0;
         for(int i=0;i<numbers.length;i++){
             int start=i;
             for(int j=i;j<numbers.length;j++){
@@ -9,10 +10,12 @@ public class print_subArrays {
                 for(int k=start;k<=end;k++){
                     System.out.print(numbers[k]+" ");
                 }
+                tr++;
                 System.out.println();
             }
             System.out.println();
         }
+        System.out.println(tr);
     }
     public static void main(String[] args) {
         int numbers[]={2,4,6,8,10};
--- 37-Question-37/Maximum_Product_Subarray_in_an_Array.java
+++ 37-Question-37/Maximum_Product_Subarray_in_an_Array.java
@@ -26,7 +26,31 @@
 //  In the given array, we can see (-4)×(-5) gives maximum product value.
 
 public class Maximum_Product_Subarray_in_an_Array {
+    public static void maximumProduct(int n[]){
+        int maxproduct=Integer.MIN_VALUE;
+        int tr=0;
+        for(int i=0;i<n.length;i++){
+            int start=i;
+            for(int j=i;j<n.length;j++){
+                int end=j;
+                int currproduct=1;
+                for(int k=start;k<=end;k++){
+                    currproduct=currproduct*n[k];
+                }
+                tr++;
+                System.out.println(currproduct);
+                if(maxproduct<currproduct){
+                    maxproduct=currproduct;
+                }
+                
+            }
+        }
+        System.out.println(maxproduct);
+        System.out.println(tr);
+    }
     public static void main(String[] args) {
+        int nums[]={1,2,3,4,5,0};
+        maximumProduct(nums);
         
     }
 }
