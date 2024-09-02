9e4e79e5f04f449e8b089fe5f3433a96eb22e480
patelprince0555 <patelprince0555@gmail.com>
2024-09-02 19:10:06 +0530

    02-09-2024

---
+++ 41-Question-41/decreasing_number.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class decreasing_number {
+    public static void printArray(Integer num[]){
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(int num[]){
+        for(int i=0;i<num.length;i++){
+            System.out.print(num[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Integer numbers[]={2,1,5,3,4};
+        Arrays.sort(numbers,Collections.reverseOrder());
+        printArray(numbers);
+
+
+        // increasing order
+        int numbers2[]={2,1,5,3,4};
+        Arrays.sort(numbers2,2,4);// ending index are not Exclusive
+        printArray(numbers2);
+
+
+        Arrays.sort(numbers,0,5,Collections.reverseOrder());
+        printArray(numbers);
+    }
+}
