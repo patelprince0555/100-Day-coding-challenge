f410009d44bfb27e4f2eaa0e58ee5e897f8c6a55
patelprince0555 <patelprince0555@gmail.com>
2024-10-31 19:35:31 +0530

    31-10-2024

--- 001-Coding-challenge-2.0/01-Type-Promotion/Type_promotion.java
+++ 001-Coding-challenge-2.0/01-Type-Promotion/Type_promotion.java
@@ -1,10 +1,20 @@
 public class Type_promotion{
     public static void main(String[] args) {
-        byte a=10;
-        short b=5;
-        char c='a';
-        char d=(char)(a+b+c);
-        System.out.println(a+b+c);
+        // byte a=10;
+        // short b=5;
+        // char c='a';
+        // char d=(char)(a+b+c);
+        // System.out.println(a+b+c);
+        // System.out.println(d);
+        // int d=30;
+
+
+
+        long a=30;
+        float b=20.25f;
+        double c=10;
+        float d=(float)(a+b+c);
         System.out.println(d);
+        System.out.println(a+b+c);
     }
 }
