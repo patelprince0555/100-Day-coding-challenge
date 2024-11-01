49af3fd704778fe4ccb24e226817b0465de41204
patelprince0555 <patelprince0555@gmail.com>
2024-11-01 13:12:29 +0530

    01-11-2024

---
+++ 001-Coding-challenge-2.0/02-Unary-operators/Unary_operators.java
@@ -0,0 +1,13 @@
+
+public class Unary_operators {
+    public static void main(String[] args) {
+        int a=6;
+        int b=a++;
+        int c=++a;
+        a++;
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(c);
+        
+    }
+}
