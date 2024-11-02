823362842adcc1978c5ada6e8e2cd08c3023b97a
patelprince0555 <patelprince0555@gmail.com>
2024-11-02 18:29:43 +0530

    02-11-2024

---
+++ 001-Coding-challenge-2.0/04-Loops/do_while_loop.java
@@ -0,0 +1,9 @@
+public class do_while_loop {
+    public static void main(String[] args) {
+        int counter=1;
+        do{
+            System.out.print(counter+" ");
+            counter++;
+        }while(counter<=10);
+    }
+}
