c12007693e5d0554f7995d3a1807ede085da2ee8
patelprince0555 <patelprince0555@gmail.com>
2024-11-02 18:09:18 +0530

    02-11-2024

---
+++ 001-Coding-challenge-2.0/03-Switch/Switch.java
@@ -0,0 +1,23 @@
+public class Switch {
+    public static void main(String[] args) {
+        int num=4;
+        switch (num) {
+            case 1: System.out.println("monday");
+                break;
+                case 2: System.out.println("tuesday");
+                break;
+                case 3: System.out.println("wednesday");
+                break;
+                case 4: System.out.println("thursday");
+                break;
+                case 5:System.out.println("friday");
+                break;
+                case 6:System.out.println("saturday");
+                break;
+                case 7:System.out.println("sunday");
+                break;
+            default:
+                System.out.println("undefined number");
+        }
+    }
+}
