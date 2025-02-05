25cba2e3e6cb409b663c247c10538754e7592438
patelprince0555 <patelprince0555@gmail.com>
2025-02-05 17:48:45 +0530

    05-02-2025

---
+++ 001-Coding-challenge-2.0/08-Array/Binary_Search.java
@@ -0,0 +1,24 @@
+public class Binary_Search {
+    public static int binarySearch(int numbers[],int key){
+        int start=0;
+        int end=numbers.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(numbers[mid]==key){
+                return mid;
+            } 
+            if(numbers[mid]<key){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={1,3,4,5,6,7,8};
+        int key =4;
+        System.out.println("index for "+ key +" is : "+ binarySearch(numbers, key));
+
+    }
+}
