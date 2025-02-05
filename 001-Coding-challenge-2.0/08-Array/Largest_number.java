1d56cdbc9e0c594a12da6bf8f506bc68004ebea2
patelprince0555 <patelprince0555@gmail.com>
2025-02-05 16:26:30 +0530

    05-02-2025

--- 001-Coding-challenge-2.0/08-Array/Largest_number.java
+++ 001-Coding-challenge-2.0/08-Array/Largest_number.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Largest_number {
+    public static int getlargestnumber(int numbers[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<numbers.length;i++){
+            if(largest<numbers[i]){
+                largest=numbers[i];
+            }
+        }
+        return largest;
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int numbers[]=new int[5];
+        System.out.println("Enter an Array");
+        for(int i=0; i<numbers.length;i++){
+            numbers[i]=sc.nextInt();
+        }
+       System.out.println("largest number is "+getlargestnumber(numbers));
     }
 }
