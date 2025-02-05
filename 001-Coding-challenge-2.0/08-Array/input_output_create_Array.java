75ea8f33ed275e829a8bbe34d0296a46073f8e4c
patelprince0555 <patelprince0555@gmail.com>
2025-02-05 16:11:59 +0530

    05-02-2025

---
+++ 001-Coding-challenge-2.0/08-Array/input_output_create_Array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class input_output_create_Array {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // int numbers[]=new int[5];
+        // System.out.println("Enter a number");
+        // for(int i=0; i<numbers.length;i++){
+        //     numbers[i]=sc.nextInt();
+        // }
+        // for(int i=0;i<numbers.length;i++){
+        //     System.out.print(numbers[i]+" ");
+        // }
+
+        // output operation
+        int numbers[]={1,2,3,89,2,3,1};
+        for(int i=0;i<numbers.length;i++){
+            System.out.println("Element at index "+i+" : "+numbers[i]);
+        }
+
+    }
+}
