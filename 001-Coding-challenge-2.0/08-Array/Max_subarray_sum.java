94c4153ad4842a6ffe2cd717b666d29a39b830ad
patelprince0555 <patelprince0555@gmail.com>
2025-02-08 20:55:09 +0530

    08-02-2024

--- 001-Coding-challenge-2.0/08-Array/Max_subarray_sum.java
+++ 001-Coding-challenge-2.0/08-Array/Max_subarray_sum.java
@@ -1,21 +1,43 @@
 public class Max_subarray_sum { 
     public static void subarraySum(int numbers[]){
-       
-        int maxsum=Integer.MIN_VALUE;
-        for(int i=0;i<numbers.length;i++){
-            for(int j=i;j<numbers.length;j++){
-                int currsum=0;
-                for(int k=i;k<=j;k++){
-                    currsum=currsum+numbers[k];
-                    System.out.println("currsum is "+currsum);
-                }
-                if(maxsum<currsum){
-                    maxsum=currsum;
-                }
+    //     int maxsum=Integer.MIN_VALUE;
+    //     for(int i=0;i<numbers.length;i++){
+    //         for(int j=i;j<numbers.length;j++){
+    //             int currsum=0;
+    //             for(int k=i;k<=j;k++){
+    //                 currsum=currsum+numbers[k];
+    //                 System.out.println("currsum is "+currsum);
+    //             }
+    //             if(maxsum<currsum){
+    //                 maxsum=currsum;
+    //             }
+    //         }
+    //     }
+    //     System.out.println("maxsum is "+maxsum);
+    // }
+
+// max subarray sum by using prefix array
+    int maxsum=Integer.MIN_VALUE;
+    int currsum;
+    int prefix[]=new int[numbers.length];
+    prefix[0]=numbers[0];
+    for(int i=1;i<prefix.length;i++){
+        prefix[i]=numbers[i]+prefix[i-1];
+    }
+    for(int i=0;i<numbers.length;i++){
+        for(int j=i;j<numbers.length;j++){
+            if(i==0){
+                currsum=prefix[j];
+            }else{
+                currsum=prefix[j]-prefix[i-1];
+            }
+            if(maxsum<currsum){
+                maxsum=currsum;
             }
         }
-        System.out.println("maxsum is "+maxsum);
     }
+    System.out.println("maxsum is "+maxsum);
+}
     public static void main(String[] args) {
         int numbers[]={1,-2,6,-1,3};
         subarraySum(numbers);
