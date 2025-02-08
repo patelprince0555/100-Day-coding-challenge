9df0c832cea9e21efd2a8450890c60ffea7e2f9b
patelprince0555 <patelprince0555@gmail.com>
2025-02-08 12:56:36 +0530

    08-02-2024

---
+++ 001-Coding-challenge-2.0/08-Array/Print_subArrays.java
@@ -0,0 +1,18 @@
+public class Print_subArrays {
+    public static void printsubArrays(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i;j<numbers.length;j++){
+                for(int k=i;k<=j;k++){
+                    System.out.print(numbers[k]);
+                }
+                System.out.print(" "+","+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int arrays[]={2,4,6,8,10};
+        printsubArrays(arrays);
+
+    }
+}
